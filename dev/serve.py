a005af15ea385a1979321f97b97552bd29d9fc76
Michael Hui <web@focw.net>
2026-04-16 16:54:42 -0700

    never cache on client side

--- dev/serve.py
+++ dev/serve.py
@@ -20,6 +20,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=str(TEMPLATES_DIR), **kwargs)
 
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def log_message(self, format, *args):
         print(f"[serve] {self.address_string()} - {format % args}")
 
